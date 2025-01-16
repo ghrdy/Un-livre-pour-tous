92dc549c619145a6e31bece197b332d707e8d486
Tim Hrdy <tim.hrdy.1@gmail.com>
2025-01-16 22:22:49 +0100

    e

--- front/src/components/worker/BookLoans.tsx
+++ front/src/components/worker/BookLoans.tsx
@@ -121,7 +121,18 @@ export default function ChildLoansDialog({
   }, [open, accessToken, child._id]);
 
   useEffect(() => {
-    fetchLoans();
+    const refreshData = async () => {
+      await fetchLoans();
+      // Update child status based on active loans
+      const hasActiveLoan = loans.length > 0;
+      if (hasActiveLoan && child.status === "possible") {
+        await updateChildStatus("retour");
+      } else if (!hasActiveLoan && child.status === "retour") {
+        await updateChildStatus("possible");
+      }
+    };
+
+    refreshData();
   }, [accessToken, child._id]);
 
   const handleAddLoanClick = async () => {
