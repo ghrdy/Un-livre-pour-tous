5877d2b6c4a2ebf12b6980f75f605a08b6ad1c26
Tim Hrdy <tim.hrdy.1@gmail.com>
2025-01-29 14:59:33 +0100

    e

--- front/src/lib/api/config.ts
+++ front/src/lib/api/config.ts
@@ -1,1 +1,1 @@
-export const API_URL = 'http://104.248.16.166:5000/api'; // 'http://31.207.39.77:5001/api' OR 'http://104.248.16.166:3000'
+export const API_URL = 'http://104.248.16.166:5001/api'; // 'http://31.207.39.77:5001/api' OR 'http://104.248.16.166:3000'
