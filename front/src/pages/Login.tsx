d462ebacf0bfe7d90d052d33e6bdc45dc0866370
Tim Hrdy <tim.hrdy.1@gmail.com>
2025-02-07 14:40:14 +0100

    Login page v2.0

--- front/src/pages/Login.tsx
+++ front/src/pages/Login.tsx
@@ -3,17 +3,12 @@ import { useNavigate } from "react-router-dom";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card";
+import { Card, CardContent } from "@/components/ui/card";
 import { toast } from "sonner";
 import { useAuth } from "@/lib/auth";
 import { API_URL } from "@/lib/api/config";
 import Logo from "../assets/Logo.svg?react";
+import Eye from "../assets/Eye.svg?react";
 
 export default function Login() {
   const navigate = useNavigate();
@@ -21,6 +16,7 @@ export default function Login() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [error, setError] = useState("");
+  const [showPassword, setShowPassword] = useState(false);
 
   const handleSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
@@ -69,19 +65,15 @@ export default function Login() {
   return (
     <div className="flex flex-col items-center justify-center min-h-[80vh]">
       <h1 className="text-5xl font-bold tracking-tight relative">
-        <Logo className="inline-block h-25 w-25 mb-2" />
+        <Logo className="inline-block h-22 w-22 mb-2" />
       </h1>
+      <div className="mb-4"></div>
       <h2 className="text-2xl font-semibold mb-4">Connectez vous</h2>
+      <div className="mb-4"></div>
       <Card className="w-full max-w-md">
-        <CardHeader>
-          <CardTitle>Connexion</CardTitle>
-          <CardDescription>
-            Entrez vos identifiants pour vous connecter
-          </CardDescription>
-        </CardHeader>
         <CardContent>
           <form onSubmit={handleSubmit} className="space-y-4">
-            <div className="space-y-2">
+            <div className="space-y-2 mt-4">
               <Label htmlFor="email">Email</Label>
               <Input
                 id="email"
@@ -93,13 +85,19 @@ export default function Login() {
             </div>
             <div className="space-y-2">
               <Label htmlFor="password">Mot de passe</Label>
-              <Input
-                id="password"
-                type="password"
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
-                required
-              />
+              <div className="relative">
+                <Input
+                  id="password"
+                  type={showPassword ? "text" : "password"}
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                  required
+                />
+                <Eye
+                  className="absolute right-3 top-1/2 transform -translate-y-1/2 cursor-pointer"
+                  onClick={() => setShowPassword(!showPassword)}
+                />
+              </div>
             </div>
             {error && <p className="text-sm text-red-500">{error}</p>}
             <div className="flex justify-end">
@@ -116,6 +114,13 @@ export default function Login() {
           </form>
         </CardContent>
       </Card>
+      <div className="mb-4"></div>
+      <h3 className="text-l">Vous n'avez pas d'acces ?</h3>
+      <div className="flex justify-end">
+        <a href="/askfor-acces" className="text-blue-500 hover:underline">
+          Demander un accès
+        </a>
+      </div>
     </div>
   );
 }
