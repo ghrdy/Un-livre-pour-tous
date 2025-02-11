2f26a2a4ba619a2c7bf0925ab27a47afc4b83f0b
Tim Hrdy <tim.hrdy.1@gmail.com>
2025-02-11 15:18:51 +0100

    e

--- front/src/pages/Login.tsx
+++ front/src/pages/Login.tsx
@@ -2,8 +2,6 @@ import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
-import { Label } from "@/components/ui/label";
-import { Card, CardContent } from "@/components/ui/card";
 import { toast } from "sonner";
 import { useAuth } from "@/lib/auth";
 import { API_URL } from "@/lib/api/config";
@@ -70,53 +68,55 @@ export default function Login() {
       <div className="mb-4"></div>
       <h2 className="text-2xl font-semibold mb-4">Connectez vous</h2>
       <div className="mb-4"></div>
-      <Card className="w-full max-w-md">
-        <CardContent>
-          <form onSubmit={handleSubmit} className="space-y-4">
-            <div className="space-y-2 mt-4">
-              <Label htmlFor="email">Email</Label>
-              <Input
-                id="email"
-                type="email"
-                value={email}
-                onChange={(e) => setEmail(e.target.value)}
-                required
-              />
-            </div>
-            <div className="space-y-2">
-              <Label htmlFor="password">Mot de passe</Label>
-              <div className="relative">
-                <Input
-                  id="password"
-                  type={showPassword ? "text" : "password"}
-                  value={password}
-                  onChange={(e) => setPassword(e.target.value)}
-                  required
-                />
-                <Eye
-                  className="absolute right-3 top-1/2 transform -translate-y-1/2 cursor-pointer"
-                  onClick={() => setShowPassword(!showPassword)}
-                />
-              </div>
-            </div>
-            {error && <p className="text-sm text-red-500">{error}</p>}
-            <div className="flex justify-end">
-              <a
-                href="/forgot-password"
-                className="text-blue-500 hover:underline"
-              >
-                Mot de passe oublié ?
-              </a>
-            </div>
-            <Button type="submit" variant="custom" className="w-full">
+      <div className="w-full max-w-md p-6 bg-white">
+        <form onSubmit={handleSubmit} className="space-y-4">
+          <Input
+            id="email"
+            type="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            placeholder="Email"
+            required
+          />
+
+          <div className="relative mb-3 w-full">
+            <Input
+              id="password"
+              type={showPassword ? "text" : "password"}
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              required
+              placeholder="Mot de passe"
+            />
+            <Eye
+              className="absolute right-3 top-1/2 transform -translate-y-1/2 cursor-pointer"
+              onClick={() => setShowPassword(!showPassword)}
+            />
+          </div>
+          {error && <p className="text-sm text-red-500">{error}</p>}
+          <div className="flex justify-end">
+            <a
+              href="/forgot-password"
+              className="text-blue-500 hover:underline"
+            >
+              Mot de passe oublié ?
+            </a>
+          </div>
+          <div className="flex justify-center">
+            <Button
+              type="submit"
+              variant="custom"
+              className="custom-button-width3"
+              style={{ marginTop: "16px" }}
+            >
               Se connecter
             </Button>
-          </form>
-        </CardContent>
-      </Card>
+          </div>
+        </form>
+      </div>
       <div className="mb-4"></div>
       <h3 className="text-l">Vous n'avez pas d'acces ?</h3>
-      <div className="flex justify-end">
+      <div className="flex justify-end mt-1">
         <a href="/request-access" className="text-blue-500 hover:underline">
           Demander un accès
         </a>
